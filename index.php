6b6a28661392a33f4b88b6fad3f6c5a144f0cd2c
hellebraz <102181996+hellebraz@users.noreply.github.com>
2022-04-06 16:59:19 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+require _DIR_ . "/vendor/autoload.php";
+
+include _DIR_ . "/includes/header.php";
+include _DIR_ . "/includes/formulario.php";
+include _DIR_ . "/includes/footer.php";
---
+++ projeto/App/includes/formulario.php
@@ -0,0 +1,55 @@
+<main>
+
+    <section>
+        <a href="index.php">
+
+            <button class="btn btn-success">volta</button>
+
+        </a>
+    </section>
+
+    <h2 class="mt-3">cadastrar vagas</h2>
+
+    <form method="post">
+
+       <div class="form-groupo">
+           <label>projeto pessoal</label>
+           <input type="text" class="form-control" name="projeto pessoal">
+        </div>
+
+        <div class="form-group">
+            <label>Descricao</label>
+           <textarea class="form-control" name="descricao" rows="5"></textarea> 
+        </div>
+
+        <div class="form-group">
+            <label>Status</label>
+
+            <div>
+                <div class="form-check form-check-inline">
+                    <label class="form-control">
+                        <input type="radio" name="ativo" value="s" checked>ATIVO
+                    </label>
+                </div>
+            
+                 <div class="form-check form-check-inline">
+                    <label class="form-control">
+                        <input type="radio" name="ativo" value="n">INATIVO
+                    </label>
+                </div>
+                    
+            </div>
+
+        </div>
+
+        <div class="form-group">
+            <button type="submit" class="btn btn-success">ENVIAR
+
+            </button>
+
+        </div>
+    
+
+</form>
+
+</main>    
